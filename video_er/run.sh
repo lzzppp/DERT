f0c67d9b354e50022e5153c3f02a43c720373f6a
lzzppp <67220779+lzzppp@users.noreply.github.com>
2020-08-11 23:28:55 +0800

    Create run.sh

---
+++ video_er/run.sh
@@ -0,0 +1,15 @@
+python main.py --dataset market1501 --epoch 40 --model_type MCAN
+ 
+python main.py --dataset market1501 --epoch 40 --model_type RGA-Net
+
+python main.py --dataset market1501 --epoch 40 --model_type RGA-Net-PA
+
+python main.py --dataset market1501 --epoch 40 --model_type ABD-Net
+
+python main.py --dataset occluded_dukeMTMC --epoch 40 --model_type MCAN
+
+python main.py --dataset occluded_dukeMTMC --epoch 40 --model_type RGA-Net
+
+python main.py --dataset occluded_dukeMTMC --epoch 40 --model_type RGA-Net-PA
+
+python main.py --dataset occluded_dukeMTMC --epoch 40 --model_type ABD-Net

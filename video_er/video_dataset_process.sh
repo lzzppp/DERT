4ac50b83a6ee0b2fbb373f19720c443775d19621
lzzppp <67220779+lzzppp@users.noreply.github.com>
2020-08-12 09:24:36 +0800

    Create video_dataset_process.sh

---
+++ video_er/video_dataset_process.sh
@@ -0,0 +1,6 @@
+# Market1501 dataset : https://www.cv-foundation.org/openaccess/content_iccv_2015/papers/Zheng_Scalable_Person_Re-Identification_ICCV_2015_paper.pdf
+# Occluded_dukeMTMC dataset : https://github.com/lightas/Occluded-DukeMTMC-Dataset
+
+# The Video-ER task data set is Market1501 and Occluded_dukeMTMC.
+# After downloading the data, unzip the Market1501 data set to ./market1501/bounding_box_train and ./market1501/bounding_box_test
+# unzip the Occluded_dukeMTMC data set to ./occluded_dukeMTMC/bounding_box_bounding_train and ./occluded_dukeMTMC/bounding_box_bounding_test
